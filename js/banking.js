d3cb30a5a743c70106f7dc449bbd053b9bcbc52a
Rejaur9131 <raju_seu@yahoo.com>
2022-02-11 22:19:16 +0600

    done with functional way

--- js/banking.js
+++ js/banking.js
@@ -15,6 +15,35 @@ function getInputValue(inputId) {
   return amountValue;
 }
 
+function updateTotalField(totalFieldId, amount) {
+  // debugger;
+  const totalElement = document.getElementById(totalFieldId);
+  const totalText = totalElement.innerText;
+  const previousTotal = parseFloat(totalText);
+  totalElement.innerText = previousTotal + amount;
+}
+
+function getCurrentBalance() {
+  const balanceTotal = document.getElementById("balance-total");
+  const balanceTotalText = balanceTotal.innerText;
+  const previousBalanceTotal = parseFloat(balanceTotalText);
+  return previousBalanceTotal;
+}
+
+function updateBalance(amount, isAdd) {
+  const balanceTotal = document.getElementById("balance-total");
+  /*const balanceTotalText = balanceTotal.innerText;
+  const previousBalanceTotal = parseFloat(balanceTotalText); */
+
+  // balanceTotal.innerText = previousBalanceTotal + depositAmount;
+  const previousBalanceTotal = getCurrentBalance();
+  if (isAdd == true) {
+    balanceTotal.innerText = previousBalanceTotal + amount;
+  } else {
+    balanceTotal.innerText = previousBalanceTotal - amount;
+  }
+}
+
 // Handle deposit
 document
   .getElementById("deposit-button")
@@ -24,19 +53,26 @@ document
     const depositAmountText = depositInput.value;
     const depositAmount = parseFloat(depositAmountText);
  */
-    const depositAmount = getInputValue("deposit-input");
 
-    // get current deposit
-    const depositTotal = document.getElementById("deposit-total");
+    // get and update deposit total
+    /*  const depositTotal = document.getElementById("deposit-total");
     const depositTotalText = depositTotal.innerText;
     const previousDepositTotal = parseFloat(depositTotalText);
     depositTotal.innerText = previousDepositTotal + depositAmount;
+ */
 
     // update balance
-    const balanceTotal = document.getElementById("balance-total");
+    /* const balanceTotal = document.getElementById("balance-total");
     const balanceTotalText = balanceTotal.innerText;
     const previousBalanceTotal = parseFloat(balanceTotalText);
-    balanceTotal.innerText = previousBalanceTotal + depositAmount;
+    balanceTotal.innerText = previousBalanceTotal + depositAmount; */
+    const depositAmount = getInputValue("deposit-input");
+    if (depositAmount > 0) {
+      updateTotalField("deposit-total", depositAmount);
+      updateBalance(depositAmount, true);
+    }
+
+    // updateBalance(depositAmount);
   });
 // Handle withdraw
 document
@@ -45,17 +81,26 @@ document
     /* const withdrawInput = document.getElementById("withdraw-input");
     const withdrawAmountText = withdrawInput.value;
     const withdrawAmount = parseFloat(withdrawAmountText); */
-    const withdrawAmount = getInputValue("withdraw-input");
 
-    // Update withdraw total
-    const withdrawTotal = document.getElementById("withdraw-total");
+    // get and update  withdraw total
+    /* const withdrawTotal = document.getElementById("withdraw-total");
     const previousWithdrawTotalText = withdrawTotal.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalText);
     withdrawTotal.innerText = previousWithdrawTotal + withdrawAmount;
+ */
 
     // update balance after withdraw
-    const balanceTotal = document.getElementById("balance-total");
-    const balanceTotalText = balanceTotal.innerText;
-    const previousBalanceTotal = parseFloat(balanceTotalText);
-    balanceTotal.innerText = previousBalanceTotal - withdrawAmount;
+    // const balanceTotal = document.getElementById("balance-total");
+    // const balanceTotalText = balanceTotal.innerText;
+    // const previousBalanceTotal = parseFloat(balanceTotalText);
+    // balanceTotal.innerText = previousBalanceTotal - withdrawAmount;
+    const withdrawAmount = getInputValue("withdraw-input");
+    const currentBalance = getCurrentBalance();
+    if (withdrawAmount > 0 && withdrawAmount <= currentBalance) {
+      updateTotalField("withdraw-total", withdrawAmount);
+      updateBalance(withdrawAmount, false);
+    } else {
+      alert("You cannot withdraw more than what you have in your account.");
+    }
+    // updateBalance(-withdrawAmount);
   });
